20fe80219cd350684ffbee507c60a53da0e6a963
한진희 <0200134hjh@gmail.com>
2025-11-15 15:00:47 +0900

    Create meta_ir.cpp

---
+++ src/meta/meta_ir.cpp
@@ -0,0 +1,22 @@
+#include "meta_ir.hpp"
+
+namespace sponge {
+
+std::shared_ptr<IRNode> IRBuilder::literal(double v) {
+    auto n = std::make_shared<IRNode>();
+    n->op = "literal";
+    n->literal = {v};
+    return n;
+}
+
+std::shared_ptr<IRNode> IRBuilder::binary(const std::string& op,
+            std::shared_ptr<IRNode> L,
+            std::shared_ptr<IRNode> R)
+{
+    auto n = std::make_shared<IRNode>();
+    n->op = op;
+    n->children = {L, R};
+    return n;
+}
+
+} // namespace sponge

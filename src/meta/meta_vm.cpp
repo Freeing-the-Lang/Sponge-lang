65ee1eb980e41db7ec7517fd9c1b32d1acd769a2
한진희 <0200134hjh@gmail.com>
2025-11-15 15:00:00 +0900

    Create meta_vm.cpp

---
+++ src/meta/meta_vm.cpp
@@ -0,0 +1,46 @@
+#include "meta_vm.hpp"
+#include <stdexcept>
+
+namespace sponge {
+
+double VM::run(const Bytecode& bc) {
+    stack.clear();
+    size_t di = 0;
+
+    for (size_t i = 0; i < bc.ops.size(); ++i) {
+        switch (bc.ops[i]) {
+            case OpCode::PUSH:
+                stack.push_back(bc.data[di++]);
+                break;
+            case OpCode::ADD: {
+                double b = stack.back(); stack.pop_back();
+                double a = stack.back(); stack.pop_back();
+                stack.push_back(a + b);
+                break;
+            }
+            case OpCode::SUB: {
+                double b = stack.back(); stack.pop_back();
+                double a = stack.back(); stack.pop_back();
+                stack.push_back(a - b);
+                break;
+            }
+            case OpCode::MUL: {
+                double b = stack.back(); stack.pop_back();
+                double a = stack.back(); stack.pop_back();
+                stack.push_back(a * b);
+                break;
+            }
+            case OpCode::DIV: {
+                double b = stack.back(); stack.pop_back();
+                double a = stack.back(); stack.pop_back();
+                stack.push_back(a / b);
+                break;
+            }
+            case OpCode::HALT:
+                return stack.back();
+        }
+    }
+    throw std::runtime_error("VM halted unexpectedly");
+}
+
+} // namespace sponge

4ac116f52a7150dba4fafbcda9d255928af51495
한진희 <0200134hjh@gmail.com>
2025-11-15 14:59:35 +0900

    Create meta_vm.hpp

---
+++ src/meta/meta_vm.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+namespace sponge {
+
+enum class OpCode : uint8_t {
+    PUSH,      // push literal
+    ADD, SUB, MUL, DIV,
+    HALT
+};
+
+struct Bytecode {
+    std::vector<OpCode> ops;
+    std::vector<double> data;
+};
+
+class VM {
+public:
+    double run(const Bytecode& bc);
+
+private:
+    std::vector<double> stack;
+};
+
+} // namespace sponge
